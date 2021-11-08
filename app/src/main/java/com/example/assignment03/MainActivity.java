ebe7101f8d23a6ed751c25ff028c6112fdbc53c2
SyedShahidArshad <bitf18a528@pucit.edu.pk>
2021-11-08 22:48:30 +0500

    Created a Hyperlink

--- app/src/main/java/com/example/assignment03/MainActivity.java
+++ app/src/main/java/com/example/assignment03/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.assignment03;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView git;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        git = findViewById(R.id.git);
+        git.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gotoUrl("https://github.com/SyedShahidArshad/Assignment03_001");
+            }
+        });
+
+    }
+    private void gotoUrl(String s){
+        Uri uri = Uri.parse(s);
+        startActivity(new Intent(Intent.ACTION_VIEW,uri));
+
     }
 }
