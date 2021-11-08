cbdbfb9de7ec8d80c5c2604a4af81f1568c10ec2
SyedShahidArshad <bitf18a528@pucit.edu.pk>
2021-11-08 23:43:03 +0500

    Added A button for Quiz

--- app/src/main/java/com/example/assignment03/MainActivity.java
+++ app/src/main/java/com/example/assignment03/MainActivity.java
@@ -6,14 +6,24 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     ImageView git;
+    private Button quiz;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        quiz = (Button) findViewById(R.id.quiz);
+        quiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
         git = findViewById(R.id.git);
         git.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,9 +33,14 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
-    private void gotoUrl(String s){
+    private void gotoUrl(String s) {
         Uri uri = Uri.parse(s);
-        startActivity(new Intent(Intent.ACTION_VIEW,uri));
+        startActivity(new Intent(Intent.ACTION_VIEW, uri));
 
     }
+    public void openActivity2(){
+        Intent intent = new Intent(this, Quiz.class);
+        startActivity(intent);
+    }
+
 }
